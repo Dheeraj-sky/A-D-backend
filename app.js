7125b5dd728adb372928f9fd2733593b41429ca8
Dheeraj kumar <dheerajgupta0651@gmail.com>
2024-04-28 15:48:36 +0530

    changed origin

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const app = express();
 const connectDB = require("./db/conn");
 const cookieParser = require("cookie-parser");
 
-connectDB();
 require("./db/conn");
 
 const Products = require("./models/productsSchema");
 const DefaultData = require("./defaultdata");
 const cors = require("cors");
 const router = require("./routes/router");
+const errorMiddleware = require("./middleware/error_middleware");
+
+const allowedOrigins = [
+  "https://bespoke-brioche-fac046.netlify.app/",
+  `${process.env.CORS_ORIGIN}`,
+  "http://localhost:3000",
+
+  // Add more origins as needed
+];
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: function (origin, callback) {
+    console.log("req is from ", origin);
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error(`Not allowed by CORS ${origin}`)); // Block the request
+    }
+  },
   methods: "GET, POST, DELETE, PATCH, HEAD",
   credentials: true,
 };
@@ -22,8 +37,12 @@ app.use(express.json());
 app.use(cookieParser(""));
 app.use(cors(corsOptions));
 app.use(router);
-const port = 8005;
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+
+// error middleware
+app.use(errorMiddleware);
+const PORT = process.env.PORT || 8005;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server is running on port ${PORT}`);
 });
 DefaultData();
